58bf064a53d10c34f20d1282977a19dabf2b4e1c
Morgan Segura <morgan@jointhebibleproject.com>
2022-03-21 16:29:45 -0700

    Implements swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,10 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { TransformInterceptor } from './transform.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const logger = new Logger();
     const app = await NestFactory.create(AppModule);
+
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalInterceptors(new TransformInterceptor());
     app.use(cookieParser());
@@ -19,6 +21,16 @@ async function bootstrap() {
         ],
         credentials: true,
     });
+    // [Swagger]
+    const config = new DocumentBuilder()
+        .setTitle('BluePrint Example')
+        .setDescription('The BluePrint API description')
+        .setVersion('1.0')
+        .addTag('test-tag')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     const port = 3001;
     await app.listen(port);
     logger.log(`Application listening on port ${port}`);
